021ab191c4ce81f603b70f0ae7fb390cb4f9150d
KomaR1 <sypachov@gmail.com>
2020-06-10 22:12:15 +0300

    Fixing error with proper nouns

--- my_app.py
+++ my_app.py
@@ -8,6 +8,8 @@ def translate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:  # if user entered "paris"
+        return data[word.title()]
     elif len(get_close_matches(word, data.keys())) > 0:
         answer = input("Did you mean %s instead? Write [y/n]\n" % get_close_matches(word, data.keys())[0])
         answer = answer.lower()
